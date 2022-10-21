5dbde88b8a8aea55c5ebffd09594ea16fa5a9e8f
katkatw <kathlynwyqe@gmail.com>
2022-10-21 16:55:11 +0900

    10/21

---
+++ src/main/webapp/WEB-INF/sql/35nameConvention.sql
@@ -0,0 +1,21 @@
+-- table 명, 컬럼명 작성 관습 
+-- 회사의 규칙 따라라
+
+-- 대소문자 구분 안함
+-- snake_lower_case
+-- your_car_name
+
+-- 우리 강의에서는 lowerCamelCase
+-- 컬럼명 : lowerCamelCase
+-- 테이블명: UpperCamelCase
+
+CREATE TABLE myTable09 (
+	yourCarName VARCHAR(255) -- 컬럼명 데이터타입
+);
+INSERT INTO myTable09 (yourCarName)
+VALUES ('tesla');
+INSERT INTO myTable09 (yourcarname)
+VALUES ('kia');
+
+SELECT * FROM myTable09;
+SELECT * FROM mytable09; -- not ok
