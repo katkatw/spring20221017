e175dad9eb54b8b0dedd533fdcf40279339e0c2e
katkatw <kathlynwyqe@gmail.com>
2022-10-26 16:44:35 +0900

    10/26

--- src/main/java/org/zerock/mapper/lecture/Mapper01.java
+++ src/main/java/org/zerock/mapper/lecture/Mapper01.java
@@ -1,9 +1,27 @@
 package org.zerock.mapper.lecture;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import org.apache.ibatis.annotations.Select;
 
 public interface Mapper01 {
 	
 	@Select("SELECT NOW()")
 	public String getCurrentTime();
+
+	@Select("SELECT CustomerName FROM w3schools.Customers WHERE CustomerId = 77")
+	public String getCustomerName();
+	
+	public String getEmployeeName();
+	
+	public String getProductName();
+	
+	public double getPrice(); // result 타입과 return 타입은 일치한다?
+
+	public int getQuantity();
+	
+//	public Date getBirthDate();
+	
+	public LocalDate getBirthDate();
 }
