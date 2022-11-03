0f615049d1313ed890f7cfa7a2533227df83df11
katkatw <kathlynwyqe@gmail.com>
2022-11-03 17:49:45 +0900

    11/03

---
+++ src/main/java/org/zerock/domain/lecture/JavaBean22.java
@@ -0,0 +1,11 @@
+package org.zerock.domain.lecture;
+
+import java.util.List;
+
+public class JavaBean22 {
+	
+	private String address;
+	private double score;
+	private List<String> phone;
+	private boolean married;
+}
