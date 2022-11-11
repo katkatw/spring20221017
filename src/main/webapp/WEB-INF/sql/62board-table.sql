5f9826519a8b7392a77c34841e5ac6ec1332fece
katkatw <kathlynwyqe@gmail.com>
2022-11-11 17:41:08 +0900

    11/11

--- src/main/webapp/WEB-INF/sql/62board-table.sql
+++ src/main/webapp/WEB-INF/sql/62board-table.sql
@@ -108,6 +108,29 @@ SELECT * FROM File ORDER BY 1 DESC;
 	ORDER BY b.id DESC;
 
 
+-- Member 테이블 만들기
+CREATE TABLE Member (
+	id VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    inserted DATETIME DEFAULT NOW()
+);
+DESC Member;
+
+SELECT * FROM Member ORDER BY inserted DESC;
+
+SELECT 
+		id,
+		password,
+		email,
+		inserted
+	FROM
+		Member
+	ORDER BY
+		id ;
+
+
+
 
 
 
